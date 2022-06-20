8f4738d4dee642e58cdf8517ddcf4c5fb372638e
Yogas <76897741+YogaswaraRama@users.noreply.github.com>
2022-06-20 17:34:26 +0800

    change lorem ipsum in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,19 +12,18 @@ export const About = (props) => {
       </Helmet>
 
       <Navbar />
-      <Hero title="About" />
+
       <div className="px-24 pt-36">
         <h1 className="text-4xl pb-5 font-bold">
           Tentang <span className="text-[#29b2ff]">Kami</span>
         </h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat blanditiis ducimus!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat blanditiis
-        ducimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat blanditiis
-        ducimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat
-        blanditiisLorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat
-        blanditiis ducimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat
-        blanditiis ducimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto excepturi, ex aut assumenda doloribus minus non amet! Fugiat enim praesentium necessitatibus dicta sint earum possimus dolore accusamus, placeat
-        blanditiis ducimus! ducimus!
+        Website&nbsp;
+        <span className="font-bold">
+          desa<span className="text-[#29b2ff]">Ku </span>
+        </span>
+        yang diprakarsai oleh para gen Z dari RT 05, Kota Balikpapan, Kalimantan Timur. Tujuan dari website ini adalah untuk mempermudah pencatatan data, baik data warga dan data keuangan warga agar nantinya semua pencatatan dapat dilakukan
+        lewat sistem, hal ini dimaksud untuk mengurangi penggunaan kertas serta fotocopy dokumen yang sebenarnya kurang efektif untuk dilakukan. Versi pertama dari website ini hanya mengakomodir pencatatan data keuangan dan data warga, dan
+        akan dikembangkan di versi-versi berikutnya.
       </div>
       <Footer />
     </>
